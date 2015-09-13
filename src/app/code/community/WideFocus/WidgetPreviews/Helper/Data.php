27aa94828065fa1b6e7e47e5bf9648879c027b6d
Ashoka de Wit <sjokki@gmail.com>
2015-09-13 12:53:12 +0200

    Add system configuration

---
+++ src/app/code/community/WideFocus/WidgetPreviews/Helper/Data.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Copyright © 2015 WideFocus. All rights reserved.
+ * http://www.widefocus.net
+ */
+/**
+ * Class WideFocus_WidgetPreviews_Helper_Data
+ *
+ * @author Ashoka de Wit <ashoka@widefocus.net> 
+ */
+class WideFocus_WidgetPreviews_Helper_Data
+    extends Mage_Core_Helper_Abstract
+{
+
+}
